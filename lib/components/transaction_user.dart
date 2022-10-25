a92c7152808a8c4f5b79de97d8098d0ff2e0c37a
Rosa <rosa.ribeiro@dippingcode.com.br>
2022-10-25 12:32:28 -0300

    adicionar 	.DS_Store 	assets/ 	lib/

---
+++ lib/components/transaction_user.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class TransactionUser extends StatefulWidget {
+  const TransactionUser({Key? key}) : super(key: key);
+
+  @override
+  State<TransactionUser> createState() => _TransactionUserState();
+}
+
+class _TransactionUserState extends State<TransactionUser> {
+  final _transactions = [
+    Transaction(
+      id: 't1',
+      title: 'Novo Tênis de Corrida',
+      value: 310.76,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Conta de Luz',
+      value: 211.30,
+      date: DateTime.now(),
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        ListTransaction(_transactions),
+      ],
+    );
+  }
+
+  static Transaction({required String id, required String title, required double value, required DateTime date}) {}
+
+  ListTransaction(List transactions) {}
+}
